8eefc98caebfacb62190648f5ea9841ac881cc27
Jason Song <i@wolfogre.com>
2019-12-09 19:40:13 +0800

    A:Makefile A:build.sh A:index.opml A:index.txt
    A  Makefile
    A  build.sh
    A  index.opml
    A  index.txt

---
+++ Makefile
@@ -0,0 +1,12 @@
+all: build commit push
+
+build:
+	./build.sh
+
+commit:
+	git add --all
+	echo "$$(git status -s | awk 'NR==1 {printf $$1":"$$2} NR!=1 {printf " "$$1":"$$2}')\n\n$$(git status -s)" | git commit -F -
+
+push:
+	git push origin master
+
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo '
+<?xml version="1.0" encoding="UTF-8"?>
+<opml version="2.0">
+<body>
+' > index.opml
+
+cat index.txt | while read LINE; do
+if [[ -n "$LINE" ]]; then
+    echo '<outline xmlUrl="'$LINE'" />' >> index.opml
+fi
+done
+
+echo '
+</body>
+</opml>
+' >> index.opml
